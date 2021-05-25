5bc0e2a1df4ad093c5bac59b6044c5cd214f054c
Davida-Natolotra <66999620+Davida-Natolotra@users.noreply.github.com>
2021-05-25 10:13:44 +0300

    Update config.php

--- config.php
+++ config.php
@@ -4,21 +4,21 @@ unset($CFG);
 global $CFG;
 $CFG = new stdClass();
 
-$CFG->dbtype    = getenv('DATABASE_TYPE');
+$CFG->dbtype    = getenv('pgsql');
 $CFG->dblibrary = 'native';
-$CFG->dbhost    = getenv('DATABASE_HOST');
-$CFG->dbname    = getenv('DATABASE_NAME');
-$CFG->dbuser    = getenv('DATABASE_USER');
-$CFG->dbpass    = getenv('DATABASE_PASSWORD');
+$CFG->dbhost    = getenv('ec2-3-234-85-177.compute-1.amazonaws.com');
+$CFG->dbname    = getenv('deetvsj6vsvuc4');
+$CFG->dbuser    = getenv('vyrxxdbdihaoby');
+$CFG->dbpass    = getenv('24951e33b2ed9d0dce579dfbe003d6df150a02df8b053a3d8c1ce4c4f564ff9e');
 $CFG->prefix    = 'mdl_';
 $CFG->dboptions = array (
   'dbpersist' => 0,
-  'dbport' => getenv('DATABASE_PORT'),
+  'dbport' => getenv('5432'),
   'dbsocket' => '',
 );
 
-$CFG->wwwroot   = getenv('WWWROOT');
-$CFG->dataroot  = getenv('DATAROOT');
+$CFG->wwwroot   = getenv('https://fiofanana.herokuapp.com/');
+$CFG->dataroot  = getenv('/tmp');
 $CFG->admin     = 'admin';
 
 $CFG->directorypermissions = 0777;
